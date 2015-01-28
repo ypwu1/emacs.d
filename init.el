65a431eb4396c9b6740eb41fe9a67509ebc03ab7
ypwu1 <oscar821210@gmail.com>
2015-01-28 18:03:34 +1100

    add gocode and compnay

--- init.el
+++ init.el
@@ -197,3 +197,27 @@
 ;; gofmt
 ;;----------------------------------------------------------------------------
 (add-hook 'before-save-hook 'gofmt-before-save)
+;;----------------------------------------------------------------------------
+;; go-compnay
+;;----------------------------------------------------------------------------
+(require 'company-go) 
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+(add-hook 'go-mode-hook (lambda ()(set (make-local-variable 'company-backends) '(company-go))(company-mode))));; Only use company-mode with company-go in go-mode
+(custom-set-faces
+ '(company-preview
+   ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common
+   ((t (:inherit company-preview))))
+ '(company-tooltip
+   ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-selection
+   ((t (:background "steelblue" :foreground "white"))))
+ '(company-tooltip-common
+   ((((type x)) (:inherit company-tooltip :weight bold))
+    (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection
+   ((((type x)) (:inherit company-tooltip-selection :weight bold))
+    (t (:inherit company-tooltip-selection)))))
